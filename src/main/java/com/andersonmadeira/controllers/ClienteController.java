b259b869cef601cb32c4084062c8d54bfcae309e
Anderson Madeira <andersonmadeiracs@gmail.com>
2019-05-01 12:33:19 -0300

    Endpoint clientes criado

---
+++ src/main/java/com/andersonmadeira/controllers/ClienteController.java
@@ -0,0 +1,26 @@
+package com.andersonmadeira.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.andersonmadeira.domain.Cliente;
+import com.andersonmadeira.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteController {
+	
+	@Autowired
+	private ClienteService service;
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente client = service.buscar(id);
+		return ResponseEntity.ok().body(client);
+	}
+	
+}
---
+++ src/main/java/com/andersonmadeira/services/ClienteService.java
@@ -0,0 +1,24 @@
+package com.andersonmadeira.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.andersonmadeira.domain.Cliente;
+import com.andersonmadeira.repositories.ClienteRepository;
+import com.andersonmadeira.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> client = repo.findById(id);
+		return client.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id = " + id
+				+ ", Tipo: " + Cliente.class.getName()));
+	}
+
+}
