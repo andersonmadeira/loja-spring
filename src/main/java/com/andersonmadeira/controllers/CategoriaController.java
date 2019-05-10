67f54af61598a0ca9b4a2f8f1163bb40e00a15f9
Anderson Madeira <andersonmadeiracs@gmail.com>
2019-05-09 23:23:21 -0300

    Inserindo nova Categoria

--- src/main/java/com/andersonmadeira/controllers/CategoriaController.java
+++ src/main/java/com/andersonmadeira/controllers/CategoriaController.java
@@ -1,11 +1,15 @@
 package com.andersonmadeira.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.andersonmadeira.domain.Categoria;
 import com.andersonmadeira.services.CategoriaService;
@@ -13,7 +17,7 @@ import com.andersonmadeira.services.CategoriaService;
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaService service;
 
@@ -22,5 +26,13 @@ public class CategoriaController {
 		Categoria cat = service.buscar(id);
 		return ResponseEntity.ok().body(cat);
 	}
-	
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+
 }
